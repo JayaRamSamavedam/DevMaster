7de8137928c4818055b7b1153bc203f892d386cf
JayaRamSamavedam <98570026+JayaRamSamavedam@users.noreply.github.com>
2023-09-30 18:40:40 +0530

    new changes

--- myproject/App/urls.py
+++ myproject/App/urls.py
@@ -7,5 +7,12 @@ from App import views
 urlpatterns = [
     # path('',include("App.urls")),
     path("",views.index,name="index"),
-    
+    path("home",views.index,name="home"),
+    path("aboutus",views.aboutus,name="aboutus"),
+    path("contact",views.contact,name="contact"),
+    path("faq",views.faq,name="faq"),
+    path("productdetail",views.productdetail,name="productdetail"),
+    path("products",views.products,name="products"),
+    path("signup",views.signup,name="signup"),
+    path("signin",views.signin,name="signin"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- myproject/App/views.py
+++ myproject/App/views.py
@@ -5,3 +5,25 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"App/index.html",{})
+
+def aboutus(request):
+    return render(request,"App/aboutus.html",{})
+
+def contact(request):
+    return render(request,"App/contact.html",{})
+
+def faq(request):
+    return render(request,"App/faq.html",{})
+
+def productdetail(request):
+    return render(request,"App/product-detail.html",{})
+
+def products(request):
+    return render(request,"App/products.html",{})
+
+def signin(request):
+    return render(request,"App/sign-in.html",{})
+
+def signup(request):
+    return render(request,"App/sign-up.html",{})
+
